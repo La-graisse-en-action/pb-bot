2725821c6275183045aaf39ae40bf22fc86e884c
Luis Alvarez <luis.alvarez@nowports.com>
2022-10-31 15:39:36 -0600

    add eslint and fix prettier and eslint issues

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,35 +1,35 @@
-const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config');
-const fs = require('node:fs');
+const { REST, Routes } = require('discord.js')
+const { clientId, guildId, token } = require('./config')
+const fs = require('node:fs')
+const path = require('path')
 
-const commands = [];
+const commands = []
 // Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync(__dirname + "/commands").filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(path.join(__dirname, '/commands')).filter((file) => file.endsWith('.js'))
 
 console.log(commandFiles)
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
+  const command = require(`./commands/${file}`)
+  commands.push(command.data.toJSON())
 }
 
 // Construct and prepare an instance of the REST module
+// eslint-disable-next-line prettier/prettier
 const rest = new REST({ version: '10' }).setToken(token);
 
 // and deploy your commands!
+// eslint-disable-next-line prettier/prettier
 (async () => {
   try {
-    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+    console.log(`Started refreshing ${commands.length} application (/) commands.`)
 
     // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands },
-    );
+    const data = await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 
-    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`)
   } catch (error) {
     // And of course, make sure you catch and log any errors!
-    console.error(error);
+    console.error(error)
   }
-})();
+})()
