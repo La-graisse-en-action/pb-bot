ea4805f47672c8278d0dae6e9c18c2ec84995fef
Luis Alvarez <luis.alvarez@nowports.com>
2023-01-15 15:09:02 -0600

    update logs func

--- src/utils/logs.js
+++ src/utils/logs.js
@@ -1,17 +1,18 @@
 const chalk = require('chalk')
 
+const log = (param) => console.log(param)
 module.exports = {
   error(message) {
     console.error(chalk.red(`${message}`))
   },
   info(message) {
-    console.log(chalk.blue(`${message}`))
+    log(chalk.blue(`${message}`))
   },
   success(message) {
-    console.log(chalk.green(`${message}`))
+    log(chalk.green(`${message}`))
   },
   warn(message) {
     const warnColor = chalk.keyword('orange')
-    console.log(warnColor(message))
+    log(warnColor(message))
   },
 }
